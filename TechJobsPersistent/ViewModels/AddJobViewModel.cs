d06ca43949bfa5ccb0c93aca09e98d2bfe7a047b
Kellie <kelliekimble@gmail.com>
2020-07-13 23:15:34 -0400

    Updated AddJobViewModel

---
+++ TechJobsPersistent/ViewModels/AddJobViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobsPersistent.Models;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddJobViewModel
+    {
+        public string Name { get; set; }
+        public string EmployerId { get; set; }
+
+        public List<SelectListItem> Employers { get; set; }
+
+
+    }
+}
